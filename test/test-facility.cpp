6d568f7ed033ed0a07417bad16deec9e73750c3b
jrandiny <jrandiny2@gmail.com>
2019-04-03 18:05:41 +0700

    Add more test

--- test/test-facility.cpp
+++ test/test-facility.cpp
@@ -24,3 +24,55 @@ TEST(Facility, isUsable){
     p.interact(*truck);
     ASSERT_FALSE(truck->isUsable());
 }  
+
+TEST(Mixer, constructor){
+    Mixer mix(5,6);
+    ASSERT_EQ(mix.getX(),5);
+    ASSERT_EQ(mix.getY(),6);
+    ASSERT_TRUE(mix.isUsable());
+    ASSERT_EQ(mix.getType(),Cell::MixerType);
+    ASSERT_TRUE(mix.isOccupied());
+}
+
+TEST(Mixer, render){
+    Mixer mix(5,6);
+    ASSERT_EQ(mix.render(),"M");
+}
+
+TEST(Truck, constructor){
+    Truck truck(3,4);
+    ASSERT_EQ(truck.getX(),3);
+    ASSERT_EQ(truck.getY(),4);
+    ASSERT_TRUE(truck.isUsable());
+    ASSERT_EQ(truck.getType(),Cell::TruckType);
+    ASSERT_TRUE(truck.isOccupied());
+}
+
+TEST(Truck, render){
+    Truck truck(5,6);
+    ASSERT_EQ(truck.render(),"D");
+}
+
+TEST(Truck, use){
+    Player p;
+    FarmAnimal* cow = new Cow(1,2,Cell::BarnType);
+    Truck truck(3,4);
+    ASSERT_TRUE(truck.isUsable());
+    p.kill(*cow);
+    p.interact(truck);
+    ASSERT_FALSE(truck.isUsable());
+}
+
+TEST(Well, constructor){
+    Well well(10,6);
+    ASSERT_EQ(well.getX(),10);
+    ASSERT_EQ(well.getY(),6);
+    ASSERT_TRUE(well.isUsable());
+    ASSERT_EQ(well.getType(),Cell::WellType);
+    ASSERT_TRUE(well.isOccupied());
+}
+
+TEST(Well, render){
+    Well well(5,6);
+    ASSERT_EQ(well.render(),"W");
+}
--- test/test-land.cpp
+++ test/test-land.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <Player.h>
 #include <Cell/Cell.h>
 #include <Cell/Land.h>
 #include <Cell/Land/Barn.h>
@@ -29,4 +30,36 @@ TEST(Land, grass){
     ASSERT_TRUE(coop->haveGrass());
     coop->removeGrass();
     ASSERT_FALSE(coop->haveGrass());
+}
+
+TEST(Barn, constructor){
+    Barn barn(10,6);
+    ASSERT_EQ(barn.getX(),10);
+    ASSERT_EQ(barn.getY(),6);
+    ASSERT_EQ(barn.getType(),Cell::BarnType);
+    ASSERT_FALSE(barn.isOccupied());
+}
+
+TEST(Barn, render){
+    Land* barn = new Barn(1,0);
+    Player p;
+    ASSERT_EQ(barn->render(),"x");
+    p.grow(*barn);
+    ASSERT_EQ(barn->render(),"@");
+}
+
+TEST(Coop, constructor){
+    Coop coop(10,6);
+    ASSERT_EQ(coop.getX(),10);
+    ASSERT_EQ(coop.getY(),6);
+    ASSERT_EQ(coop.getType(),Cell::CoopType);
+    ASSERT_FALSE(coop.isOccupied());
+}
+
+TEST(Coop, render){
+    Land* coop = new Coop(1,0);
+    Player p;
+    ASSERT_EQ(coop->render(),"o");
+    p.grow(*coop);
+    ASSERT_EQ(coop->render(),"*");
 }
